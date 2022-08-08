5529c4c3cec1aedb7c2d00592c2ddb533132e3c1
AC59852 <austin.caron1@gmail.com>
2022-08-08 06:15:51 -0400

    added landing page

---
+++ src/components/CarouselComponent.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="landing__item">
+    <div class="landing__title">
+      <h1>{{selectedContent.title}}</h1>
+      <h2>{{selectedContent.subtitle}}</h2>
+    </div>
+    <img :src="`${this.selectedContent.icon}`" :alt="`Icon for ${selectedContent.title} ${selectedContent.subtitle}`">
+    <p>{{selectedContent.para}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['selectedContent'],
+
+  methods: {
+  }
+}
+</script>
+
+<style>
+
+</style>
